c39c0fff058e10eb3d072a4e0326483b22ff95e5
Julia <yulia.samartseva@gmail.com>
2021-11-20 15:09:12 +0200

    Add a Configuration server using Okta

---
+++ task-project/config/src/test/java/knu/groupproject/config/ConfigApplicationTests.java
@@ -0,0 +1,11 @@
+package knu.groupproject.config;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
