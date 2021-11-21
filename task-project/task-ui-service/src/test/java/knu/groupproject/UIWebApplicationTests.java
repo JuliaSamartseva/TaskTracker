b319c693000c478c6c14ac031ea1d73fd9326a7f
Marina <montag.marinka@gmail.com>
2021-11-21 14:54:13 +0200

    Use Docker to package microservices. Add Docker Compose to run everything.

---
+++ task-project/task-ui-service/src/test/java/knu/groupproject/UIWebApplicationTests.java
@@ -0,0 +1,20 @@
+package knu.groupproject;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@TestPropertySource(locations="classpath:test.properties")
+public class UIWebApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
