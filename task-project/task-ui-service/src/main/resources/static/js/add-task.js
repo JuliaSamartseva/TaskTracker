7ba528bdfc6761943e7ebef58db79c112fc539ad
Julia <yulia.samartseva@gmail.com>
2021-12-08 14:59:41 +0200

    Form validation

--- task-project/task-ui-service/src/main/resources/static/js/add-task.js
+++ task-project/task-ui-service/src/main/resources/static/js/add-task.js
@@ -1,27 +1,51 @@
+$(function() {
+    $("form[id='addTask']").validate({
+        rules: {
+            name: "required",
+            description: "required",
+            priority: "required",
+            deadline: "required"
+        },
+        messages: {
+            firstname: "Please enter task name",
+            description: "Please enter task description",
+            priority: "Please choose priority",
+            deadline: "Please choose a deadline"
+        }
+    });
+});
+
 $("#addTask").submit(function(e) {
     e.preventDefault();
     let form = $(this);
-    let url = form.attr('action');
-    var config = {};
-    jQuery(form).serializeArray().map(function(item) {
-        if ( config[item.name] ) {
-            if ( typeof(config[item.name]) === "string" ) {
-                config[item.name] = [config[item.name]];
+    if (form.valid()) {
+        let url = form.attr('action');
+        var config = {};
+        jQuery(form).serializeArray().map(function(item) {
+            if ( config[item.name] ) {
+                if ( typeof(config[item.name]) === "string" ) {
+                    config[item.name] = [config[item.name]];
+                }
+                config[item.name].push(item.value);
+            } else {
+                config[item.name] = item.value;
             }
-            config[item.name].push(item.value);
-        } else {
-            config[item.name] = item.value;
-        }
-    });
+        });
 
-    $.ajax({
-        type: "POST",
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        url: url,
-        data: JSON.stringify(config),
-        dataType: 'json'
-    });
+        $.ajax({
+            type: "POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            url: url,
+            data: JSON.stringify(config),
+            success: function() {
+                window.location.href = "/";
+            },
+            error: function() {
+                alert('Error occurred');
+            }
+        });
+    }
 });
