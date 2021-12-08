626c8279a09f9f9594e4fcc4e15dc9406f9cdf8c
Julia <yulia.samartseva@gmail.com>
2021-12-08 16:35:52 +0200

    Create navbar

--- task-project/task-ui-service/src/main/java/knu/groupproject/controller/TaskController.java
+++ task-project/task-ui-service/src/main/java/knu/groupproject/controller/TaskController.java
@@ -12,11 +12,13 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
+
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
--- task-project/task-ui-service/target/classes/static/js/add-task.js
+++ task-project/task-ui-service/target/classes/static/js/add-task.js
@@ -1,4 +1,4 @@
-$(function() {
+$(function () {
     $("form[id='addTask']").validate({
         rules: {
             name: "required",
@@ -15,35 +15,37 @@ $(function() {
     });
 });
 
-$("#addTask").submit(function(e) {
+$("#addTask").submit(function (e) {
     e.preventDefault();
     let form = $(this);
-    let url = form.attr('action');
-    var config = {};
-    jQuery(form).serializeArray().map(function(item) {
-        if ( config[item.name] ) {
-            if ( typeof(config[item.name]) === "string" ) {
-                config[item.name] = [config[item.name]];
+    if (form.valid()) {
+        let url = form.attr('action');
+        var config = {};
+        jQuery(form).serializeArray().map(function (item) {
+            if (config[item.name]) {
+                if (typeof (config[item.name]) === "string") {
+                    config[item.name] = [config[item.name]];
+                }
+                config[item.name].push(item.value);
+            } else {
+                config[item.name] = item.value;
             }
-            config[item.name].push(item.value);
-        } else {
-            config[item.name] = item.value;
-        }
-    });
+        });
 
-    $.ajax({
-        type: "POST",
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        url: url,
-        data: JSON.stringify(config),
-        success: function() {
-            window.location.href = "/";
-        },
-        error: function() {
-            alert('Error occurred');
-        }
-    });
+        $.ajax({
+            type: "POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            url: url,
+            data: JSON.stringify(config),
+            success: function () {
+                window.location.href = "/";
+            },
+            error: function () {
+                alert('Error occurred');
+            }
+        });
+    }
 });
